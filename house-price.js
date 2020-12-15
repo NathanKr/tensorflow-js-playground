da4e1516af12a457c821fdb33a0a3096d2ff2f1e
Nathan Krasney <natankrasney@gmail.com>
2020-12-15 14:54:54 +0200

    working two samples

--- house-price.js
+++ house-price.js
@@ -1,7 +1,6 @@
 console.log("app is loading ...");
 const tf = require("@tensorflow/tfjs");
 
-
 // Define a model for linear regression.
 const model = tf.sequential();
 model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
@@ -10,11 +9,12 @@ model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
 model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
 
 // Generate some synthetic data for training.
-const xs = tf.tensor1d([1, 2, 3, 4]);
-const ys = tf.tensor1d([1, 3, 5, 7]);
+// house price is 50K + 50 per bed room
+const xs = tf.tensor1d([0,1, 2, 3, 4]);
+const ys = tf.tensor1d([50, 100, 150, 200, 250]);
 
 // Train the model using the data.
 model.fit(xs, ys,{epochs: 1000}).then(() => {
   // Use the model to do inference on a data point the model hasn't seen before:
-  model.predict(tf.tensor1d([5])).print(); // -----> y=2x-1 fit exactly
+  model.predict(tf.tensor1d([7])).print(); //
 });
